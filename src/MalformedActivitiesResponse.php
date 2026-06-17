e33feeb83cc3c28331019cd8e2cb3ae2802d2e3b
Felix Ruthenberg <felix.ruthenberg@outlook.com>
2026-06-17 05:40:21 +0000

    use phpcs

--- src/MalformedActivitiesResponse.php
+++ src/MalformedActivitiesResponse.php
@@ -4,4 +4,6 @@ declare(strict_types=1);
 
 namespace Kmc\Eversports;
 
-final class MalformedActivitiesResponse extends \RuntimeException {}
+final class MalformedActivitiesResponse extends \RuntimeException
+{
+}
